2cc4b8230a2d50e7f599d37e98d4884b220e8adc
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-01-09 09:23:36 -0600

    Going throught Lesson 4 just finished the add_page_to_index

---
+++ Lesson4/add_page_to_index_procedure.py
@@ -0,0 +1,30 @@
+# !usr/bin/env python
+
+# Code from 
+# Introduction to Computer Programming in Udacity
+# Lesson 4
+
+# First define an empty list
+index = []
+
+# Reusing a existing procedure
+def add_to_index(index,keyword,url):
+	# check if the keyword already exist
+	for entry in index:
+		# if first word match
+		if entry[0] == keyword:
+			# append the url at the end
+			entry[1].append(url)
+			# exit
+			return
+	# if it doesn't exists
+	index.append([keyword,[url]])
+
+def add_page_to_index(index,url,content):
+	# Get every word in the page content
+	words = content.split()
+	for eWord in words:
+		add_to_index(index,eWord,url)
+
+add_page_to_index(index,'fake.text',"This is a test")
+print index
