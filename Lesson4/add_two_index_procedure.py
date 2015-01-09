08ce22adec0a7779481dbc76ab6598e865ba9362
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-01-08 20:40:41 -0600

    Beginning with the documentation for the course Introduction to computer programming Udacity

---
+++ Lesson4/add_two_index_procedure.py
@@ -0,0 +1,23 @@
+# !usr/bin/env python
+
+# Code from 
+# Introduction to Computer Programming in Udacity
+
+index = []
+
+def add_to_index(index,keyword,url):
+	# check if the keyword already exist
+	for entry in index:
+		# if first word match
+		if entry[0] == keyword:
+			# append the url at the end
+			entry[1].append(url)
+			# exit
+			return
+	# if it doesn't exists
+	index.append([keyword,[url]])
+
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print index
