ec6c5668c2ec3eaef180d5521d1d7c34bc3dfa64
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-01-12 09:30:01 -0600

    Finish with hashtables and starting using dictionaries

---
+++ Lesson5/dictionary_declaration.py
@@ -0,0 +1,12 @@
+# !usr/bin/env python
+
+# Code from 
+# Introduction to Computer Science in Udacity
+# Lesson 5
+
+# define a dictionary with the population of different cities
+population = { 'Shanghai':17.8 , 'Istanbul':13.3 , 
+			   'Karachi':13.0 , 'Mumbai':12.5 }
+
+# test code
+print population
